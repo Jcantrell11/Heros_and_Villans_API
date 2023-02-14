8ef24d741de7c26b2f048f67ad9275ff06228779
Jcantrell11 <118132470+Jcantrell11@users.noreply.github.com>
2023-02-14 00:06:31 -0500

    Updated views.py GET method + Created POST method

--- supers/views.py
+++ supers/views.py
@@ -1,13 +1,28 @@
+from django.shortcuts import get_list_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import SuperSerializer
+from rest_framework import status
 from .models import Super
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    supers = Super.objects.all()
-
-    serializer = SuperSerializer(supers, many=True)
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        super_type_type = request.query_params.get('super_type')
+        print(super_type_type)
+        queryset = Super.objects.all()
+        
+        if super_type_type:
+            queryset = queryset.filter(super_type__type=super_type_type)
+        
+        serializer = SuperSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 

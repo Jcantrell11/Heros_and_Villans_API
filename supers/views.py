14ffaafc15424b7907e1dc8bb8560c3e05deea28
Jcantrell11 <118132470+Jcantrell11@users.noreply.github.com>
2023-02-14 19:54:57 -0500

    Added super_type_id to Supers serializers.py + added super_detail function to views.py

--- supers/views.py
+++ supers/views.py
@@ -11,7 +11,6 @@ def supers_list(request):
     
     if request.method == 'GET':
         super_type_type = request.query_params.get('super_type')
-        print(super_type_type)
         queryset = Super.objects.all()
         
         if super_type_type:
@@ -32,4 +31,12 @@ def super_detail(request, pk):
     if request.method == 'GET':
         serializer = SuperSerializer(super)
         return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 

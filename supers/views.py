74198b72a3cb14e3f74f3d2ed1effc2511d8eba0
Jcantrell11 <118132470+Jcantrell11@users.noreply.github.com>
2023-02-21 22:10:38 -0500

    Added postman collection

--- supers/views.py
+++ supers/views.py
@@ -4,26 +4,37 @@ from rest_framework.response import Response
 from .serializers import SuperSerializer
 from rest_framework import status
 from .models import Super
-
+from super_types.models import SuperType
 
 @api_view(['GET', 'POST'])
 def supers_list(request):
-    
     if request.method == 'GET':
-        super_type_type = request.query_params.get('super_type')
-        queryset = Super.objects.all()
+        super_types = SuperType.objects.all()
+        custom_response_dictionary = {}
+
+        for super_type in super_types:
+
+            supers = Super.objects.filter(super_type_id=super_type.id)
+
+            super_serializer = SuperSerializer(supers, many=True)
+
+            custom_response_dictionary[super_type.type] = {
+                "supers":super_serializer.data
+            }
+
+        return Response(custom_response_dictionary)
         
-        if super_type_type:
-            queryset = queryset.filter(super_type__type=super_type_type)
-        
+    elif super_type:
+        queryset = queryset.filter(super__type=super_type)
         serializer = SuperSerializer(queryset, many=True)
         return Response(serializer.data)
+
     elif request.method == 'POST':
-        
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
     
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
